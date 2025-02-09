f7c54320dbeb94c57235d1c85adde0e115eab91d
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-02-09 18:18:31 +0800

    added notes/rules

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,6 @@
+// First 2 exports are just references for future utility functions to be added.
 export { _objectMapper } from './_objectMapper';
 export { _sayHello } from './_sayHello';
+
+// RULES
+// Strictly follows camel-casing for both function and file names
