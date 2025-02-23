45368824bed1979e1af4fe2eed29c6e0ad911683
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-02-23 18:12:13 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 // First 2 exports are just references for future utility functions to be added.
-export { _objectMapper } from './_objectMapper';
+export { _objectMapper } from './_objectMapper/index.js';
 export { _sayHello } from './_sayHello';
 export { formatCurrency } from './formatCurrency';
 export { isEmpty } from './isEmpty';
