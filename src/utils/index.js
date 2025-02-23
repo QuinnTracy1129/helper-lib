d23ca4e94dff4ec485d55c00eaaf6d2e08d8d257
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-02-23 18:13:37 +0800

    Update index.js
    Added .js to everything to fix backend import issue

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 // First 2 exports are just references for future utility functions to be added.
 export { _objectMapper } from './_objectMapper/index.js';
-export { _sayHello } from './_sayHello';
-export { formatCurrency } from './formatCurrency';
-export { isEmpty } from './isEmpty';
+export { _sayHello } from './_sayHello.js';
+export { formatCurrency } from './formatCurrency.js';
+export { isEmpty } from './isEmpty.js';
 
 // RULES
 // Strictly follows camel-casing for both function and file names
