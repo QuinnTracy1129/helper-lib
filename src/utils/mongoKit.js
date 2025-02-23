bf182137f87f08dc005f7a7feeab171feb62fea7
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-02-23 19:52:11 +0800

    Update mongoKit.js

--- src/utils/mongoKit.js
+++ src/utils/mongoKit.js
@@ -1,27 +1,28 @@
-// import { isEmpty } from './isEmpty.js';
+import { isEmpty } from './isEmpty.js';
 
-const filter = async (Model, criteria, options = { sort: { createdAt: -1 }, select: '' }) => {
-  try {
-    const { select, sort } = options;
+// const filter = async (Model, criteria, options = { sort: { createdAt: -1 }, select: '' }) => {
+//   try {
+//     const { select, sort } = options;
 
-    const payload = await Model.find(criteria).select(`${select} -password`).sort(sort).lean();
+//     const payload = await Model.find(criteria).select(`${select} -password`).sort(sort).lean();
 
-    return {
-      //   code: isEmpty(payload) ? 204 : 200,
-      code: 200,
-      payload,
-    };
-  } catch (message) {
-    return {
-      code: 500,
-      payload: {
-        error: 'INTERNAL_SERVER_ERROR',
-        message,
-      },
-    };
-  }
-};
+//     return {
+//       code: isEmpty(payload) ? 204 : 200,
+//       payload,
+//     };
+//   } catch (message) {
+//     return {
+//       code: 500,
+//       payload: {
+//         error: 'INTERNAL_SERVER_ERROR',
+//         message,
+//       },
+//     };
+//   }
+// };
 
-export const mongoKit = {
-  filter,
-};
+export async function mongoKit() {
+  return {
+    hello: 'world',
+  };
+}
