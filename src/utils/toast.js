853a200efcbcaf1fa7994c2338ccb24ac286f658
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-02-25 16:10:09 +0800

    Update toast.js

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -2,7 +2,7 @@ import Swal from 'sweetalert2';
 
 export function toast(settings) {
   const darkMode = Boolean(Number(localStorage.getItem('darkMode'))),
-    { light = {}, dark = {} } = JSON.parse(localStorage.getItem() || '{}');
+    { light = {}, dark = {} } = JSON.parse(localStorage.getItem('toastSettings') || '{}');
 
   let background = light.background || '#783F8E',
     color = light.color || '#C8C6D7';
