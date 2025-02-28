1bfc505027bde1aad455340c4d1f610a6f91a63f
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-03-01 01:08:50 +0800

    Update mongoReferenceExists.js

--- src/utils/mongoReferenceExists.js
+++ src/utils/mongoReferenceExists.js
@@ -1,5 +1,5 @@
 export async function mongoReferenceExists(_id, modelName) {
-  const referenceExists = this.model(modelName).findById(_id);
+  const referenceExists = await this.model(modelName).findById(_id);
 
   if (!referenceExists) throw new Error(`[${_id}] Does not exist in ${modelName}.`);
 }
