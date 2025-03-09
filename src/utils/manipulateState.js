da7ba92ced7274239883e425027e5aa1fc42575b
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-03-09 15:15:38 +0800

    Update manipulateState.js

--- src/utils/manipulateState.js
+++ src/utils/manipulateState.js
@@ -1,16 +1,14 @@
 // used to manipulate redux state when updating or deleting
-export function manipulateState(state, payload) {
+export function manipulateState(state, payload, shouldDelete = false) {
   const statesToBeManipulated = ['storage', 'collection'];
 
-  // string type is for deletion
-  if (typeof payload === 'string') {
+  if (shouldDelete) {
     for (const key of statesToBeManipulated) {
       const index = state[key].findIndex(({ _id }) => _id === payload?._id);
 
       if (index > -1) state[key].splice(index, 1);
     }
   } else {
-    // object type is for update
     for (const key of statesToBeManipulated) {
       const index = state[key].findIndex(({ _id }) => _id === payload?._id);
 
