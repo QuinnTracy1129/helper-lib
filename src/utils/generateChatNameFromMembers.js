77bddd5bebbf083d27241d10d4d07389bffb317d
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-03-17 20:54:18 +0800

    Update generateChatNameFromMembers.js

--- src/utils/generateChatNameFromMembers.js
+++ src/utils/generateChatNameFromMembers.js
@@ -1,14 +1,15 @@
 import { formatFullname } from './formatFullname.js';
 
 export function generateChatNameFromMembers(creator, members, auth) {
-  // if we only have 1 member, this is just a PM
-  if (members.length === 1) {
-    if (creator?._id === auth?._id) return formatFullname(members[0]?.details?.fullName);
+  // if we only have 2 members, this is just a PM
+  if (members.length === 2) {
+    // return the member that is NOT the user
+    const member = members.filter(({ details }) => details?._id !== auth?._id);
 
-    return formatFullname(creator?.fullName);
+    return formatFullname(member[0]?.details?.fullName);
   }
 
-  // if you are the creator, generate name from members
+  // if there are more than 2 members
   if (creator?._id === auth?._id)
     return members.map(({ details }) => details?.fullName?.fname).join(', ');
 
