79149f4a65f020c04ee442ff679af4d74d05ea8f
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-06-18 15:44:44 +0800

    created data table

---
+++ src/components/dataTable/index.jsx
@@ -0,0 +1,18 @@
+// DataTable Features:
+// Keyword search
+// Ability to paginate data
+// Sort items per column
+
+// Cell Features:
+// can pass texts to be outputted
+// format outputs
+// make them clickable
+// can output custom components or html
+// can pass custom CSS
+export function DataTable() {
+  return (
+    <div>
+      <button className="btn btn-primary">Hello</button>
+    </div>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 // Strictly follows camel-casing for both function and file names
 export { useDebounce } from './useDebouce.jsx';
 export { useToggle } from './useToggle.jsx';
+export { DataTable } from './dataTable/index.jsx';
