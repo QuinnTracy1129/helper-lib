a91839caeaf7c987aac6396f68a0a30504f8aee4
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-06-22 18:25:29 +0800

    Update index.js

--- src/constants/philippineAddresses/index.js
+++ src/constants/philippineAddresses/index.js
@@ -2,11 +2,17 @@ import regions from './1_regions';
 import provinces from './2_provinces';
 import cityMun from './3_city-mun';
 import barangays from './4_barangays';
+import { isEmpty } from '../../utils/isEmpty.js';
 
 const getCodeByName = (data, name) => data.find((item) => item.name === name)?.code;
 
-const filterByParentCode = (data, objectKey, parentCode) =>
-  data.filter((item) => item[objectKey] === parentCode);
+const filterByParentCode = (data, objectKey, parentCode) => {
+  const result = data.filter((item) => item[objectKey] === parentCode);
+
+  if (isEmpty(result)) return [{ name: '--', code: '--' }];
+
+  return result;
+};
 
 export default {
   getRegions: () => regions,
