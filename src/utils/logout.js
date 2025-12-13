73e000d446de662496bc765227ddd5443387dac4
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2025-12-13 17:38:35 +0800

    Update logout.js

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -2,6 +2,7 @@ export function logout() {
   localStorage.removeItem('activePermission');
   localStorage.removeItem('token');
   localStorage.removeItem('email');
+  localStorage.removeItem('isLoggedIn');
   localStorage.removeItem('authenticationFailed');
   window.location.href = '/';
 }
