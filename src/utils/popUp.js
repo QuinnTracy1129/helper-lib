9d08c2032765e66ccb502f7d7f4624238fe6dc32
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2026-01-25 16:45:09 +0800

    Update popUp.js

--- src/utils/popUp.js
+++ src/utils/popUp.js
@@ -1,35 +1,31 @@
 import Swal from 'sweetalert2';
 
 export function popUp(func = () => true, options = {}) {
-  const { body, confirmButtonText, config } = options;
+  const {
+    title = 'Are you sure?',
+    body,
+    confirmButtonText,
+    cancelButtonText = 'Cancel',
+    icon = 'question',
+  } = options;
 
-  const swalWithBootstrapButtons = Swal.mixin(config);
-
-  swalWithBootstrapButtons
-    .fire({
-      customClass: {
-        actions: 'flex gap-x-5',
-        confirmButton: 'btn btn-primary w-40',
-        cancelButton: 'btn btn-ghost w-40',
-      },
-      buttonsStyling: false,
-      title: '<strong>Are you sure?</strong>',
-      icon: 'question',
-      html: `
-        <p className="py-4">${body}</p>
-  `,
-      showCloseButton: true,
-      showCancelButton: true,
-      focusConfirm: false,
-      reverseButtons: true,
-      confirmButtonText: `
-    ${confirmButtonText} 
-  `,
-      cancelButtonText: `
-    Cancel 
-  `,
-    })
-    .then(({ isConfirmed }) => {
-      if (isConfirmed) func();
-    });
+  Swal.fire({
+    customClass: {
+      actions: 'flex gap-x-5',
+      confirmButton: 'btn btn-primary w-40',
+      cancelButton: 'btn btn-ghost w-40',
+    },
+    buttonsStyling: false,
+    title: `<strong>${title}</strong>`,
+    icon,
+    html: `<p className="py-4">${body}</p>`,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: true,
+    reverseButtons: true,
+    confirmButtonText,
+    cancelButtonText,
+  }).then(({ isConfirmed }) => {
+    if (isConfirmed) func();
+  });
 }
