0c1261a4ba3a2563a05b5a1ef41045e8959ec88e
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2026-01-25 16:40:25 +0800

    Update popUp.js

--- src/utils/popUp.js
+++ src/utils/popUp.js
@@ -7,6 +7,14 @@ export function popUp(func = () => true, options = {}) {
 
   swalWithBootstrapButtons
     .fire({
+      customClass: {
+        customClass: {
+          actions: 'flex gap-x-5',
+          confirmButton: 'btn btn-primary w-40',
+          cancelButton: 'btn btn-ghost w-40',
+        },
+        buttonsStyling: false,
+      },
       title: '<strong>Are you sure?</strong>',
       icon: 'question',
       html: `
