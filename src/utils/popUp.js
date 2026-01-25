c8eceb8b5c8d97d5f63acc5f764dba48c380c162
Benedict Earle Gabriel Romero Pajarillaga <benedictearle@gmail.com>
2026-01-25 14:14:17 +0800

    Update popUp.js

--- src/utils/popUp.js
+++ src/utils/popUp.js
@@ -2,23 +2,32 @@ import Swal from 'sweetalert2';
 
 export function popUp(func = () => true, options = {}) {
   const { body, confirmButtonText } = options;
-  Swal.fire({
-    title: '<strong>Are you sure?</strong>',
-    icon: 'question',
-    html: `
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-primary w-1/2',
+      cancelButton: 'btn btn-ghost w-full',
+    },
+    buttonsStyling: false,
+  });
+  swalWithBootstrapButtons
+    .fire({
+      title: '<strong>Are you sure?</strong>',
+      icon: 'question',
+      html: `
         <p className="py-4">${body}</p>
   `,
-    showCloseButton: true,
-    showCancelButton: true,
-    focusConfirm: false,
-    reverseButtons: true,
-    confirmButtonText: `
-    <button class="btn btn-primary w-1/2">${confirmButtonText}</button> 
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      reverseButtons: true,
+      confirmButtonText: `
+    <button class=">${confirmButtonText}</button> 
   `,
-    cancelButtonText: `
-    <button class="btn btn-ghost w-full">Cancel</button> 
+      cancelButtonText: `
+    <button class="">Cancel</button> 
   `,
-  }).then(({ isConfirmed }) => {
-    if (isConfirmed) func();
-  });
+    })
+    .then(({ isConfirmed }) => {
+      if (isConfirmed) func();
+    });
 }
